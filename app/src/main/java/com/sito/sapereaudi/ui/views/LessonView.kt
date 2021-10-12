edad074307f9d674e664527ea946438453cabf77
SitoNimbus <sito8943@gmail.com>
2021-10-12 12:00:41 -0700

    views changed to new components

--- app/src/main/java/com/sito/sapereaudi/ui/views/LessonView.kt
+++ app/src/main/java/com/sito/sapereaudi/ui/views/LessonView.kt
@@ -1,34 +1,18 @@
 package com.sito.sapereaudi.ui.views
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sito.sapereaudi.R
-import com.sito.sapereaudi.ui.theme.Salmon
+import com.sito.sapereaudi.ui.components.header.Header
+import com.sito.sapereaudi.ui.components.header.HeaderRow
+import com.sito.sapereaudi.ui.components.layout.MenuColumn
 
 @Composable
 fun LessonView() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Salmon),
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 25.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
+    MenuColumn {
+        HeaderRow {
+            Header(
                 text = stringResource(id = R.string.lessonsTitle),
-                color = Color.White,
-                fontSize = 50.sp
             )
         }
     }
