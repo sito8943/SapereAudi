d00408a8d8c859f7f0a797766589d39024fa89b8
SitoNimbus <sito8943@gmail.com>
2021-10-12 11:01:29 -0700

    doing components

---
+++ app/src/main/java/com/sito/sapereaudi/ui/components/ButtonText.kt
@@ -0,0 +1,19 @@
+package com.sito.sapereaudi.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ButtonText(content: @Composable (() -> Unit)?=null) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        content
+    }
+}
--- app/src/main/java/com/sito/sapereaudi/ui/views/MainMenu.kt
+++ app/src/main/java/com/sito/sapereaudi/ui/views/MainMenu.kt
@@ -6,23 +6,27 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sito.sapereaudi.R
-import com.sito.sapereaudi.ui.theme.DarkBlue
+import com.sito.sapereaudi.ui.components.ButtonRow
+import com.sito.sapereaudi.ui.theme.ButtonSalmon
 import com.sito.sapereaudi.ui.theme.Salmon
 
 @Composable
-fun MainMenu() {
+fun MainMenu(onClick:(Int)->Unit?) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Salmon),
+        verticalArrangement = Arrangement.SpaceAround
     ) {
         Row(
             modifier = Modifier
@@ -45,17 +49,24 @@ fun MainMenu() {
             Button(
                 modifier = Modifier
                     .padding(5.dp)
-                    .background(color = DarkBlue),
-                onClick = { /*TODO*/ }) {
-                Image(
-                    modifier = Modifier.padding(5.dp),
-                    painter = painterResource(id = R.drawable.book),
-                    contentDescription = ""
-                )
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = stringResource(id = R.string.historyButton)
-                )
+                    .background(color = ButtonSalmon)
+                    .size(width = 250.dp, height = 70.dp),
+                onClick = { onClick(0)}) {
+                ButtonRow() {
+                    Image(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(width = 50.dp, height = 50.dp),
+                        painter = painterResource(id = R.drawable.book),
+                        contentDescription = ""
+                    )
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = stringResource(id = R.string.historyButton),
+                        color = Color.White,
+                        fontSize = 14.sp
+                    )
+                }
             }
         }
         Row(
@@ -67,18 +78,28 @@ fun MainMenu() {
             Button(
                 modifier = Modifier
                     .padding(5.dp)
-                    .background(color = Color.Blue),
+                    .background(color = Color.Blue)
+                    .size(width = 250.dp, height = 70.dp),
                 onClick = { /*TODO*/ }) {
-                Image(
-                    modifier = Modifier.padding(5.dp),
-                    painter = painterResource(id = R.drawable.mynotebook),
-                    contentDescription = ""
-                )
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = stringResource(id = R.string.lessonsButton),
-                    color = Color.White
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(width = 50.dp, height = 50.dp),
+                        painter = painterResource(id = R.drawable.mynotebook),
+                        contentDescription = ""
+                    )
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = stringResource(id = R.string.lessonsButton),
+                        color = Color.White,
+                        fontSize = 14.sp
+                    )
+                }
             }
         }
         Row(
@@ -90,17 +111,28 @@ fun MainMenu() {
             Button(
                 modifier = Modifier
                     .padding(5.dp)
-                    .background(color = Color.Blue),
+                    .background(color = Color.Blue)
+                    .size(width = 250.dp, height = 70.dp),
                 onClick = { /*TODO*/ }) {
-                Image(
-                    modifier = Modifier.padding(5.dp),
-                    painter = painterResource(id = R.drawable.writemachine),
-                    contentDescription = ""
-                )
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = stringResource(id = R.string.naturaButton)
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(width = 50.dp, height = 50.dp),
+                        painter = painterResource(id = R.drawable.writemachine),
+                        contentDescription = ""
+                    )
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = stringResource(id = R.string.naturaButton),
+                        color = Color.White,
+                        fontSize = 14.sp
+                    )
+                }
             }
         }
         Row(
@@ -112,17 +144,28 @@ fun MainMenu() {
             Button(
                 modifier = Modifier
                     .padding(5.dp)
-                    .background(color = Color.Blue),
+                    .background(color = Color.Blue)
+                    .size(width = 250.dp, height = 70.dp),
                 onClick = { /*TODO*/ }) {
-                Image(
-                    modifier = Modifier.padding(5.dp),
-                    painter = painterResource(id = R.drawable.portrait),
-                    contentDescription = ""
-                )
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = stringResource(id = R.string.authorsButton)
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(width = 50.dp, height = 50.dp),
+                        painter = painterResource(id = R.drawable.portrait),
+                        contentDescription = ""
+                    )
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = stringResource(id = R.string.authorsButton),
+                        color = Color.White,
+                        fontSize = 14.sp
+                    )
+                }
             }
         }
     }
