62b2a61999b12a5a16e90113312ca3f98f4c24e6
SitoNimbus <sito8943@gmail.com>
2021-10-12 12:23:13 -0700

    onclick working

--- app/src/main/java/com/sito/sapereaudi/ui/views/MainActivity.kt
+++ app/src/main/java/com/sito/sapereaudi/ui/views/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.sito.sapereaudi.ui.views
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.sito.sapereaudi.ui.theme.SapereAudiTheme
 
@@ -14,17 +15,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val context=LocalContext.current as Activity
             SapereAudiTheme {
-                MainMenu(fun(value: Int){
-
-                })
+                MainMenu { id ->
+                    mainMenuClickHandler(context = context , id)
+                }
             }
         }
     }
 }
 
+fun mainMenuClickHandler(context: Context, who: Int) {
+    Toast.makeText(context,who.toString(),Toast.LENGTH_SHORT).show()
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-
+    SapereAudiTheme {
+       // MainMenu(mainMenuClickHandler())
+    }
 }
--- app/src/main/java/com/sito/sapereaudi/ui/views/MainMenu.kt
+++ app/src/main/java/com/sito/sapereaudi/ui/views/MainMenu.kt
@@ -1,13 +1,11 @@
 package com.sito.sapereaudi.ui.views
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
